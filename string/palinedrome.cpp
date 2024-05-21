f388ac254f89a47e8c4a92398ca85292e6884276
Al Abir <sheikhabir196@gmail.com>
2024-05-21 08:19:46 +0600

    string

---
+++ string/palinedrome.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include<string.h>
+
+using namespace std;
+class Solution{
+    public:
+    bool isPalindrome(string s){
+          int start=0;
+         int end=s.size()-1;
+       while(start<=end){
+           if(!isalnum(s[start])){start++; continue;}
+           if(!isalnum(s[end])){end--;continue;}
+           if(tolower(s[start])!=tolower(s[end]))return false;
+           else{
+               start++;
+               end--;
+           }
+       }
+       return true;
+    }
+};
+int main()
+{
+    
+     string s ="madam";
+     Solution solution;
+     bool ans = solution.isPalindrome(s);
+
+     if(ans){
+           cout<<"Palinedrome "<<endl;
+     }else{
+        cout<<"Not Palinedrome "<<endl;
+     }
+
+
+
+
+
+
+    return 0;
+}
